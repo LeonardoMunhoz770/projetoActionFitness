bb9020ebba929e03ac7307ab606c96b071d22c4f
unknown <ssfa91@comp91.ssfanet.com.br>
2022-10-06 16:17:05 -0300

    Adicionando funcoes de redirecionamento na pagina de planos

--- screens/plans/index.js
+++ screens/plans/index.js
@@ -12,10 +12,35 @@ document.querySelector("#showButton").addEventListener("click", function(){
 })
 
 
-document.querySelector("#selectPlano").addEventListener("click", function(){7
+
+const selectPlanoBasico = () =>{
     plano = document.querySelector("#plano").textContent
     const urlParams = new URLSearchParams(window.location.search)
     urlParams.set('plano', `${plano}`)
     window.location.search = urlParams
     window.location.href = `../registration/matricula/index.html?${urlParams}`
-})
+}
+
+const selectPlanoCompleto = () =>{
+    plano = document.querySelector("#planoCompleto").textContent
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('plano', `${plano}`)
+    window.location.search = urlParams
+    window.location.href = `../registration/matricula/index.html?${urlParams}`
+}
+
+const selectPlanoFamiliarBasico = () =>{
+    plano = document.querySelector("#planoFamiliarBasico").textContent
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('plano', `${plano}`)
+    window.location.search = urlParams
+    window.location.href = `../registration/matricula/index.html?${urlParams}`
+}
+
+const selectPlanoFamiliarCompleto = () =>{
+    plano = document.querySelector("#planoFamiliarCompleto").textContent
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('plano', `${plano}`)
+    window.location.search = urlParams
+    window.location.href = `../registration/matricula/index.html?${urlParams}`
+}
