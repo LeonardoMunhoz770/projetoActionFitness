289c078c6c0b095ad13669d9074a84a80e091e8d
unknown <ssfa91@comp91.ssfanet.com.br>
2022-10-06 14:23:35 -0300

    Enviando user para tela de menu via query

---
+++ screens/menu/index.js
@@ -0,0 +1,18 @@
+
+//Busca via query o parâmetro passado de login e imprimi na tela num spam
+function armazenarUsuario(){ 
+    let query = location.search.slice(1);
+    let partes = query.split('&');
+    let data = {};
+    partes.forEach(function (parte) {
+        let chaveValor = parte.split('=');
+        let chave = chaveValor[0];
+        let valor = chaveValor[1];
+        let user = atob(valor)
+        data[chave] = user;
+        document.querySelector("#user").innerHTML += `${user}`
+    });
+}
+armazenarUsuario()
+
+
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+
 const validarAcesso = () =>{
+    //Busca o que foi digitado no campo login e senha
     const login = document.querySelector("#login").value;
     const senha = document.querySelector("#senha").value;
 
-    
+    //Verifica se os campos estão vazios.
     if(login.length == 0 || senha.length == 0){
 
         console.log('Campos vázio. por favor preencha todos os campos')
-
-        
-    }else{
+ 
+    }
+        //Envia o login e senha para o backend realizar a validação
+    else{
         fetch("https://reqres.in/api/login", {
             method: 'POST',
             headers:{
@@ -20,23 +23,26 @@ const validarAcesso = () =>{
             })
         }).then((response) =>{
             if(response.status !== 200){
-                function limparCampos(){
-                    document.querySelector("#alerta").remove()
-                }
-                let alerta = `<p class="alerta" id="alerta">Credenciais incorretas!</p>`
-                let erroCredenciais = document.querySelector("#containerInputs")
-                erroCredenciais.innerHTML += alerta;
-
-                setTimeout(limparCampos, 1000)
-                
+                console.log("error")
                 
             }else{
-                window.location.href = './screens/menu/index.html'
+                enviarLogin()
+                
             }
         }).catch((error) =>{
             console.log(`Erro: ${error}`)
         })
     }
+}
+
+//Envia o user via query com encode para utilizar na tela de menu na opção usuário logado e avança para a próxima tela.
+const enviarLogin = () =>{
+    let user = document.querySelector("#login").value;
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('user', `${btoa(user)}`)
+    window.location.search = urlParams
+    window.location.href = `./screens/menu/index.html?${urlParams}`
+
 }
 
 document.querySelector("#validar").addEventListener("click", function(){
