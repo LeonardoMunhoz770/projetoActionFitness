9ab440572781a88128c6e0de6e359faeefa29475
LeonardoMunhoz770 <munhozleonardo756@gmail.com>
2022-10-07 11:03:05 -0300

    Adicionando fetch para banco de dados e estilizacao de loading

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+async function reqLogin(){
+    const login = document.querySelector("#login").value;
+    const senha = document.querySelector("#senha").value;
+    let loading = document.querySelector(".lds-ring")
+    loading.style.display = 'flex'
+
+    await fetch("http://127.0.0.1:3000/login", {
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json'        
+        },
+        body: JSON.stringify({
+            login: login, 
+            senha: senha
+        })
+    }).then((response) =>{
+        if(response != true){
+            function noLoading(){
+                loading.style.display = 'none'
+                
+            }
+            setTimeout(noLoading, 3000)
+        }else{
+            function noLoading(){
+                loading.style.display = 'none'
+                enviarLogin()
+            }
+            setTimeout(noLoading, 3000)
+        }
+        
+    }).finally(() =>{
+        
+    })
+}
 
 const validarAcesso = () =>{
     //Busca o que foi digitado no campo login e senha
@@ -6,32 +40,11 @@ const validarAcesso = () =>{
 
     //Verifica se os campos estão vazios.
     if(login.length == 0 || senha.length == 0){
-
         console.log('Campos vázio. por favor preencha todos os campos')
- 
     }
         //Envia o login e senha para o backend realizar a validação
-    else{
-        fetch("https://reqres.in/api/login", {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: login,
-                password: senha
-            })
-        }).then((response) =>{
-            if(response.status !== 200){
-                console.log("error")
-                
-            }else{
-                enviarLogin()
-                
-            }
-        }).catch((error) =>{
-            console.log(`Erro: ${error}`)
-        })
+    else{    
+        reqLogin()
     }
 }
 
